a9d89b4287e87917c34fee91cec98b3c94bfbc25
Adarsh1442005 <btech60077.23@bitmesra.ac.in>
2025-06-18 02:56:34 +0530

    Update calling.js

--- src/calling.js
+++ src/calling.js
@@ -84,7 +84,7 @@ const stream=await navigator.mediaDevices.getUserMedia({video:true,audio:false})
   };
   peerRef.current=peer;
   if(candi){
-    peerRef.current.addIceCandidate(new RTCIceCandidate(candidate));
+    peerRef.current.addIceCandidate(new RTCIceCandidate(candi));
   }
 
 
