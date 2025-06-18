78df8716fdc13957e9d9876893e5f35c48f29535
Adarsh1442005 <btech60077.23@bitmesra.ac.in>
2025-06-18 17:55:10 +0530

    Update calling.js

--- src/calling.js
+++ src/calling.js
@@ -346,7 +346,7 @@ return (
           Accept
         </button>
         <button
-         onClick={rejectcall};
+         onClick={rejectcall}
           className="bg-red-500 hover:bg-red-600 text-white font-bold py-2 px-4 rounded shadow"
         >
           Reject
