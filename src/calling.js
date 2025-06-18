ed78528dc1d88e2c40bec8849a34e8a2df1d4df1
Adarsh1442005 <btech60077.23@bitmesra.ac.in>
2025-06-18 16:21:15 +0530

    Update calling.js

--- src/calling.js
+++ src/calling.js
@@ -233,7 +233,7 @@ return (
             </div>
           </>
         ) }
-       {registered && !caller && !receiver && !connect && !callerrec && reg && (
+       {registered && reg && !caller && !receiver && !connect && !callerrec && (
           <div>
             <p className="text-cyan-300 mb-4 text-sm">
               Logged in as: <span className="font-medium">{userid}</span>
